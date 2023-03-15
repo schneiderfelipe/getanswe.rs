c81ec91b8c0717104ac3768f9494fa4bc16bab59
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-03-15 18:12:52 -0300

    docs(answer): use ticks

--- answer/src/main.rs
+++ answer/src/main.rs
@@ -3,7 +3,7 @@
 //! [![Crates.io license](https://img.shields.io/crates/l/answer)](https://github.com/schneiderfelipe/answer/blob/main/LICENSE)
 //! [![Build CI](https://github.com/schneiderfelipe/answer/actions/workflows/ci.yml/badge.svg)](https://github.com/schneiderfelipe/answer/actions/workflows/ci.yml)
 //! [![Changelog CI](https://github.com/schneiderfelipe/answer/actions/workflows/changelog.yml/badge.svg)](https://github.com/schneiderfelipe/answer/blob/main/CHANGELOG.md#changelog)
-//! [![Libraries.io SourceRank](https://img.shields.io/librariesio/sourcerank/cargo/answer)](https://libraries.io/cargo/answer)
+//! [![Libraries.io `SourceRank`](https://img.shields.io/librariesio/sourcerank/cargo/answer)](https://libraries.io/cargo/answer)
 //!
 //! > `answer` _any_ question right from your terminal,
 //! > using the same
