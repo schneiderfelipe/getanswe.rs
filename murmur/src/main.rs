3d5cb41a07cf8bc7eb407a6b813eacc80188c0f6
Felipe S. S. Schneider <schneider.felipe.5@gmail.com>
2023-03-19 00:31:54 -0300

    revert(murmur): remove support for jack

--- murmur/src/main.rs
+++ murmur/src/main.rs
@@ -18,61 +18,11 @@ use cpal::{
 
 #[derive(Parser, Debug)]
 #[command(version, about = "CPAL record_wav example", long_about = None)]
-struct Opt {
-    /// The audio device to use
-    #[arg(short, long, default_value_t = String::from("default"))]
-    device: String,
-
-    /// Use the JACK host
-    #[cfg(all(
-        any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd"
-        ),
-        feature = "jack"
-    ))]
-    #[arg(short, long)]
-    #[allow(dead_code)]
-    jack: bool,
-}
+struct Opt {}
 
 fn main() -> Result<(), anyhow::Error> {
-    let opt = Opt::parse();
+    let _opt = Opt::parse();
 
-    // Conditionally compile with jack if the feature is specified.
-    #[cfg(all(
-        any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd"
-        ),
-        feature = "jack"
-    ))]
-    // Manually check for flags. Can be passed through cargo with -- e.g.
-    // cargo run --release --example beep --features jack -- --jack
-    let host = if opt.jack {
-        cpal::host_from_id(cpal::available_hosts()
-            .into_iter()
-            .find(|id| *id == cpal::HostId::Jack)
-            .expect(
-                "make sure --features jack is specified. only works on OSes where jack is available",
-            )).expect("jack host unavailable")
-    } else {
-        cpal::default_host()
-    };
-
-    #[cfg(any(
-        not(any(
-            target_os = "linux",
-            target_os = "dragonfly",
-            target_os = "freebsd",
-            target_os = "netbsd"
-        )),
-        not(feature = "jack")
-    ))]
     let host = cpal::default_host();
 
     println!(
@@ -84,13 +34,13 @@ fn main() -> Result<(), anyhow::Error> {
     );
 
     // Set up the input device and stream with the default input config.
-    let device = if opt.device == "default" {
-        host.default_input_device()
+    let device = if let Some(device) = host.default_input_device() {
+        device
     } else {
         host.input_devices()?
-            .find(|x| x.name().map(|y| y == opt.device).unwrap_or(false))
-    }
-    .expect("failed to find input device");
+            .next()
+            .expect("failed to find input device")
+    };
 
     println!("Input device: {}", device.name()?);
 
